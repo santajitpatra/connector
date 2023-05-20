9ef3db160ee7765a3b1c0e943f8f7e5cf4a24c8e
SantajitPatra <business.santajitpatra@gmail.com>
2023-05-20 23:50:09 +0530

    init : with cli

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+            <h1 className='text-blue-500 bg-slate-800'>hi</h1>
+
+    </div>
+  )
+}
+
+export default Header
